21b6327f3b250cf816f8bd2b86493cf5bde9560c
Maciej Grula <maciej.grula@execon.pl>
2020-02-12 11:48:24 +0100

    env setting

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        filename: 'eservice-pl.js', // <-- Important
+        path: path.resolve(__dirname, 'dist'),
+        // ,
+        // libraryTarget: 'this' // <-- Important
+        library: 'eservice',
+        libraryTarget: 'umd',
+        globalObject: 'this'
+    },
+    //target: 'node', // <-- Important
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.tsx?$/,
+    //             //loader: 'ts-loader',
+    //             options: {
+    //                 //transpileOnly: true
+    //             }
+    //         }
+    //     ]
+    // },
+    resolve: {
+        extensions: [ '.js' ]
+    },
+    externals: {jointjs: "joint"}
+    // ,
+    // externals: [nodeExternals({
+    //     whitelist: ['paytip-module-tipbox','paytip-module-core', 'paytip-module-wallet', 'paytip-module-integration','paytip-module-integration-inbound','paytip-module-bo']
+    // })] // <-- Important
+};
